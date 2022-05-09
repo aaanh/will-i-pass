871abdd62be774a78134aa5d5016b913080494b4
Anh <iam@hoanganh.tech>
2022-05-09 02:38:31 -0400

    refactor header, restyle dropdown

--- web-app/components/Branding.js
+++ web-app/components/Branding.js
@@ -51,7 +51,7 @@ export default function Branding() {
 					by <Link href="https://github.com/aaanh">Anh Hoang Nguyen</Link>
 				</p>
 				<div className="flex justify-between space-x-4">
-					<button
+					{/* <button
 						onClick={(e) => {
 							e.preventDefault();
 							Router.push("https://www.hoanganh.dev/");
@@ -76,10 +76,10 @@ export default function Branding() {
 						className="border-b border-transparent hover:border-b-sky-500"
 					>
 						GitHub Repo
-					</button>
+					</button> */}
 					<Menu as="div" className="relative inline-block text-left">
 						<div>
-							<Menu.Button className="inline-flex w-full justify-center rounded-md items-center text-lg dark:bg-black bg-opacity-20 px-4 py-2 font-medium dark:text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+							<Menu.Button className="inline-flex w-full justify-center rounded-md items-center text-sm dark:bg-black bg-opacity-20 px-4 py-2 font-medium dark:text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
 								Other Projects
 								<ChevronDownIcon
 									className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
@@ -96,7 +96,7 @@ export default function Branding() {
 							leaveFrom="transform opacity-100 scale-100"
 							leaveTo="transform opacity-0 scale-95"
 						>
-							<Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y dark:bg-slate-900 dark:text-white divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+							<Menu.Items className="absolute left-0 mt-2 w-56 origin-top-right divide-y dark:bg-slate-900 dark:text-white divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 								<div className="px-1 py-1 ">
 									<Menu.Item>
 										{({ active }) => (
